3a50bf518aab2e0216de0034a5c49b9d8bc4ecf0
user530 <m.timarsuev@gmail.com>
2023-08-01 12:53:59 +0300

    small clean up

--- src/testres/dto/update-testre.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTestreDto } from './create-testre.dto';
-
-export class UpdateTestreDto extends PartialType(CreateTestreDto) {}
--- src/testres/testres.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TestresService } from './testres.service';
-import { TestresController } from './testres.controller';
-
-@Module({
-  controllers: [TestresController],
-  providers: [TestresService]
-})
-export class TestresModule {}
